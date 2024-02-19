3c6bc43724fa2c0a7eaa1a778232e11e2c2e18e2
rohitchoudhary99 <rohit.choudhary@softradix.in>
2024-02-19 17:10:35 +0530

    show platform icon

---
+++ src/components/gameCard/PlatformsIconsList.tsx
@@ -0,0 +1,37 @@
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa'
+import {MdPhoneIphone} from 'react-icons/md'
+import {SiNintendo} from 'react-icons/si'
+import {BsGlobe} from 'react-icons/bs'
+import { IconType } from "react-icons";
+
+import { platformInterfcae } from '../../hooks/UseGame'
+import { HStack, Icon } from '@chakra-ui/react'
+
+interface platformIconsProps {
+    plateformItemsList: platformInterfcae[]
+}
+
+export const PlatformsIconsList = ({ plateformItemsList }: platformIconsProps) => {
+    const iconMap: {[key:string] : IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        andriod : FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+    return (
+        <>
+        <HStack marginY={1}>
+            {
+                plateformItemsList.map((platform) => (
+                   <Icon as={iconMap[platform.slug]} color='gray.500'/>
+                ))
+            }
+            </HStack>
+        </>
+    )
+}
