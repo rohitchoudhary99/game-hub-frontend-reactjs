a9294d982d2780e2820e4b8fb9babf0ac4f669a2
rohitchoudhary99 <rohit.choudhary@softradix.in>
2024-02-20 12:05:37 +0530

    in this branch cretae a common get api call when pasge refresh

--- src/hooks/UseGenres.ts
+++ src/hooks/UseGenres.ts
@@ -1,33 +1,12 @@
 import { useEffect, useState } from "react"
 import apiClient from "../services/api-client"
+import UseData from "./UseData"
 
 interface genres {
     id: number,
     name: string
 }
 
-interface fetchGenersResponse {
-    count: number,
-    results: genres[]
-}
+ const UseGenres = () => UseData<genres>('/genres')
 
-export const UseGenres = () => {
-    const [genres, setGenres] = useState<genres[]>([])
-    const [error, setError] = useState('')
-    const [isLoading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        apiClient.get<fetchGenersResponse>('/genres')
-            .then(({data}) => {
-                setGenres(data.results)
-                setLoading(false)
-            })
-            .catch((error) => {
-                setError(error.message)
-                setLoading(false)
-            })
-    }, [])
-
-    return {genres, error, isLoading}
-}
+ export default UseGenres
