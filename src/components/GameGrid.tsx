7208c2731031cc76cc5cecd3e74dffc667220fb4
rohitchoudhary99 <rohit.choudhary@softradix.in>
2024-02-21 17:30:04 +0530

    handle the functionlty if game have no image thesn show the dummy image

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,12 +8,11 @@ import { GameCardSkeketon } from './gameCard/GameCardSkeketon'
 
 interface props {
     gameQuery : gameQuery
-
 }
 export const GameGrid = ({ gameQuery }: props) => {
     const skeleton = [1, 2, 3, 4, 5, 6, 7];
     const { data, error, isLoading } = UseGame(gameQuery)
-    console.log(gameQuery.sortOrder)
+
     return (
         <>
             <SimpleGrid columns={{ sm: 1, md: 2., lg: 3, xl: 5 }} spacing={10} paddingTop='34px' alignItems='stretch'>
