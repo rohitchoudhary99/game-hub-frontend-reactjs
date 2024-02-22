e40670ecf2dca4bc8b05afee1a0c827ef2a7d2f8
rohitchoudhary99 <rohit.choudhary@softradix.in>
2024-02-22 12:18:05 +0530

    console error fixed

--- src/components/gameCard/GameCardSkeketon.tsx
+++ src/components/gameCard/GameCardSkeketon.tsx
@@ -1,9 +1,12 @@
 import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
-export const GameCardSkeketon = () => {
+interface skeleton {
+  id : number
+}
+export const GameCardSkeketon = ({id}:skeleton) => {
   return (
     <>
-    <Card>
+    <Card key={id}>
         <Skeleton height='200px' />
         <CardBody>
             <SkeletonText></SkeletonText>
